813a0c17c987e3e5540e485ef12a23e7276dd0e0
Fred Videon <fred@cs.washington.edu>
2017-09-27 14:01:21 -0700

    Initial commit

---
+++ js/nav-bar.js
@@ -0,0 +1,27 @@
+$(function(){    
+    // Assuming the nav bar is in the parent directory, 
+    // load it, make modifications, and
+    // insert it into the page header.
+    $("header").load("../nav-bar.html", "", function() {
+        
+        // Attempt to set the "active" class on the currently selected item.
+        // This toggles the menu item's background color.
+        // It is looking for a part of the URL that matches a non-empty href string
+        // of a menu item.  If a match is found, that item is considered active.
+        var thishref = $(location).attr('href');
+        var tokens = thishref.split("/");
+        $(".navbar-nav a[href]").each(function(){
+            $(this).parent().removeClass('active');
+            var href = $(this).attr('href');
+            if ((href !== '') && ($.inArray(href,tokens) > 0)) {
+                $(this).parent().addClass('active');
+            }           
+        });
+        
+        // Prepend '../' to each href.
+        $(".navbar-nav a[href]").attr("href", function(i, href){
+            return "../" + href;
+        });
+        
+    });
+});
