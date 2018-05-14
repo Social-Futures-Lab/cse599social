f757d7a8dcb026ec7146136fac91c6c9193e0379
Fred Videon <fred@cs.washington.edu>
2018-05-14 15:40:57 -0700

    Restore the content of previous commit into the site subdirectory.

---
+++ site/js/footer.js
@@ -0,0 +1,12 @@
+$(function(){    
+    // Assuming the footer is in the parent directory, 
+    // load it, modify img src, prepending "../", and
+    // insert into the page footer.
+    $("footer").load("../site/footer.html", "", function() {
+        $("img").attr("src", function(i, href){
+            return "../" + href;
+        });
+    });
+});    
+
+
